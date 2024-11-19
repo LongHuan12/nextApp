fc4edd5bfdcbad94e4495845bf83bb7451dccf7d
kneknew <son11032001@gmail.com>
2024-11-19 21:37:23 +0700

    Add migrations and apply changes for reservation model. Update ReservationSidebar

--- myApp/app/properties/[id]/page.tsx
+++ myApp/app/properties/[id]/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
 
 import apiService from "@/app/services/apiService";
+import {getUserId} from "@/app/lib/actions";
 
 const PropertyDetailPage = async ({params}: { params: {id: string }}) => {
     const property = await apiService.get(`/api/properties/${params.id}`);
+    const userId = await getUserId();
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
             <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
@@ -49,6 +51,7 @@ const PropertyDetailPage = async ({params}: { params: {id: string }}) => {
 
                 <ReservationSidebar
                     property={property}
+                    userId={userId}
 
                 />
             </div>
