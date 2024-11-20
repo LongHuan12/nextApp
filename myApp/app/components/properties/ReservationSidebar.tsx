84f1fa01d19e66a07a37feeb87af1213f76f6ca9
kneknew <son11032001@gmail.com>
2024-11-20 20:08:01 +0700

    Create calendar & add to ReservationSidebar.

--- myApp/app/components/properties/ReservationSidebar.tsx
+++ myApp/app/components/properties/ReservationSidebar.tsx
@@ -2,10 +2,10 @@
 
 import {useState, useEffect} from "react";
 import {Range} from 'react-date-range';
-
+import {differenceInDays, eachDayOfInterval} from 'date-fns';
+import DatePicker from "@/app/components/forms/Calendar";
 import apiService from "@/app/services/apiService";
 import useLoginModal from "@/app/hooks/useLoginModal";
-import {differenceInDays, eachDayOfInterval} from 'date-fns';
 
 const initialDateRange = {
     startDate: new Date(),
@@ -38,6 +38,22 @@ const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
     const [guests, setGuests] = useState<string>('1');
     const guestsRange = Array.from ({ length: property.guests}, (_, index) => index + 1)
 
+    const _setDateRange = (selection: any) => {
+        const newStartDate = new Date(selection.startDate);
+        const newEndDate = new Date(selection.endDate);
+
+        if (newEndDate <= newStartDate) {
+            newEndDate.setDate(newStartDate.getDate() + 1);
+        }
+
+        setDateRange({
+            ...dateRange,
+            StartDate: newStartDate,
+            endDate: newEndDate
+        })
+
+    }
+
     useEffect(() => {
         if (dateRange.startDate && dateRange.endDate) {
             const dayCount = differenceInDays(
@@ -63,6 +79,11 @@ const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
     return (
         <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
             <h2 className="mb-5 text-2xl">{property.price_per_night} per night</h2>
+
+            <DatePicker
+                value={dateRange}
+                onChange={(value) => setDateRange(value.selection)}
+            />
 
             <div className="mb-6 p-3 border border-gray-400 rounded-xl">
                 <label className="mb-2 block font-bold text-xs">Guests</label>
